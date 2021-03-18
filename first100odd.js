b144904ae3b3e283c20dd5d9e907ec3901e83705
Nandini Raut <rautnandini2001@gmail.com>
2021-03-18 15:19:58 +0530

    code to print first 100 odd numbers

---
+++ first100odd.js
@@ -0,0 +1,15 @@
+// print first 100 odd numbers
+// using while loop
+
+let i = 0;
+while(i <= 200){
+  if (i % 2 ==! 0) console.log(i);
+  i++
+}
+
+let j =0;
+do{
+    if (i % 2 ==! 0) console.log(i);
+  i++
+} while(i <= 200)
+ 
