227b5374e599731e9a87e79b6f91195ef7edd512
Nandini Raut <rautnandini2001@gmail.com>
2021-03-21 21:57:28 +0530

    js code for executing operators

---
+++ operators.js
@@ -0,0 +1,22 @@
+//ternary operator
+let point = 110;
+let type = point > 100 ? "gold" : "silver";
+console.log(type);
+
+//logical operators
+console.log(true&&true);
+console.log(true||false);
+let loanAccepted = true;
+let ApllicationRefused = !loanAccepted;
+console.log(ApllicationRefused);
+
+//swapping integers
+let a = "red";
+let b = "blue";
+let c = a;
+a = b;
+b = c;
+console.log(a);
+console.log(b);
+
+//if else statements
