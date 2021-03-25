e0eb0363fd45491fff2c4b7e87cb0759f4e6e0fa
Nandini Raut <rautnandini2001@gmail.com>
2021-03-25 14:12:23 +0530

    clonning object

---
+++ cloningobject.js
@@ -0,0 +1,18 @@
+const circle = {
+    radius: 1,
+    draw(){
+        console.log("draw");
+    }
+};
+
+// const another = {};
+// for (let key in circle)
+// another[key] = circle[key];
+
+// const another = Object.assign({
+//     color: 'yellow'
+// }, circle);
+
+const another = { ...circle};
+
+console.log(another);
