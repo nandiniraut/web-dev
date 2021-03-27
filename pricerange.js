9c48e0c688d6e1f4f9c557a2fecd77a39598c220
Nandini Raut <rautnandini2001@gmail.com>
2021-03-27 22:40:34 +0530

    price range

---
+++ pricerange.js
@@ -0,0 +1,9 @@
+let priceranges = [
+    { label : '$', tooltip: 'inexpeensive', minPerPerson: 0, maxPerPerson: 10},
+    { label : '$$', tooltip: 'moderate', minPerPerson: 11, maxPerPerson: 20},
+    { label : '$', tooltip: 'expeensive', minPerPerson: 21, maxPerPerson: 30}
+];
+
+let restaurants = [
+    {avgperperson: 5}
+];
