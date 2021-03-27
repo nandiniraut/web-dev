0cf07a81dd0ec62016c1102823cc7c88fe504bca
Nandini Raut <rautnandini2001@gmail.com>
2021-03-27 22:40:06 +0530

    blog post object

---
+++ blogpostobject.js
@@ -0,0 +1,14 @@
+let post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 'd',
+    comments: [
+        {author: 'a', body: 'b'},
+        {author: 'c', body: 'd'}
+    ],
+       
+    isLive: 'true'
+}
+
+console.log(post);
