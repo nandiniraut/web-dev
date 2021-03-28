953f6700edc79c8a6e656de328373dd0319d222d
Nandini Raut <rautnandini2001@gmail.com>
2021-03-28 21:52:17 +0530

    edit

--- pricerange.js
+++ pricerange.js
@@ -1,4 +1,4 @@
-let priceranges = [
+ilet priceranges = [
     { label : '$', tooltip: 'inexpeensive', minPerPerson: 0, maxPerPerson: 10},
     { label : '$$', tooltip: 'moderate', minPerPerson: 11, maxPerPerson: 20},
     { label : '$', tooltip: 'expeensive', minPerPerson: 21, maxPerPerson: 30}
